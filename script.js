9d3a865837405bdb4771f8ac4e9f7adca9aa1170
thewhitehaven04 <thewhitehaven04@gmail.com>
2022-05-03 01:23:01 +0300

    fixed up error handling behavior

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
 const formInputs = document.querySelector('p.margin-bot-50');
 
-const validationState = (function () {
-  const state = {
-    isSamePassword: false,
-    hasCapitals: false,
-    isAboveMinimalLength: false,
+const state = {
+  isSamePassword: false,
+  hasCapitals: false,
+  isAboveMinimalLength: false,
+};
+
+const paragraphPasswordInputs = document.querySelector('main p:last-of-type');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm_password');
+const validationRoot = document.querySelector('.validation-messages');
+
+const samePassword = () => {
+  state.isSamePassword = password.value == confirmPassword.value;
+};
+
+const containsCapitals = () => {
+  state.hasCapitals = password.value.toLowerCase() != password.value;
+};
+
+const aboveMinimalLength = () => {
+  state.isAboveMinimalLength =
+    password.value.length > password.getAttribute('minlength');
+};
+
+const clean = function () {
+  const rootChildren = Array.from(validationRoot.children);
+  rootChildren.forEach((child) => validationRoot.removeChild(child));
+};
+
+const render = function () {
+  /** The map between error codes and their respective messages */
+  const stateMessageMapping = {
+    isSamePassword: 'Password do not match',
+    isAboveMinimalLength: 'The password must be longer than 8 characters',
+    hasCapitals: 'The password must contain at least one capital letter',
   };
 
-  const paragraphPasswordInputs = document.querySelector('main p:last-of-type');
-  const password = document.querySelector('#password');
-  const confirmPassword = document.querySelector('#confirm_password');
-  const validationRoot = document.querySelector('.validation-messages');
+  const conditions = document.createElement('ul');
+  for (const prop in state) {
+    // if one of the states is false, print the corresponding error message
+    if (!state[prop]) {
+      const li = document.createElement('li');
 
-  const samePassword = () => {
-    state.isSamePassword = password.value == confirmPassword.value;
-  };
+      // style error messages
+      li.classList.add('red');
+      li.classList.add('list-cross-marker');
 
-  const containsCapitals = () => {
-    state.hasCapitals = password.value.toLowerCase() != password.value;
-  };
-
-  const aboveMinimalLength = () => {
-    state.isAboveMinimalLength =
-      password.value.length > password.getAttribute('minlength');
-  };
-
-  const clean = function () {
-    const rootChildren = Array.from(validationRoot.children);
-    rootChildren.forEach((child) => validationRoot.removeChild(child));
-  };
-
-  const render = function () {
-    clean();
-
-    const stateMessageMapping = {
-      isSamePassword: 'Password do not match',
-      isAboveMinimalLength: 'The password must be longer than 8 characters',
-      hasCapitals: 'The password must contain at least one capital letter',
-    };
-
-    const conditions = document.createElement('ul');
-    for (const prop in state) {
-      if (!state[prop]) {
-        const li = document.createElement('li');
-
-        // style error messages
-        li.classList.add('red');
-        li.classList.add('list-cross-marker');
-
-        // append the message text
-        li.innerText = stateMessageMapping[prop];
-        conditions.appendChild(li);
-      }
+      // append the message text
+      li.innerText = stateMessageMapping[prop];
+      conditions.appendChild(li);
     }
-    validationRoot.append(conditions);
-  };
+  }
+  validationRoot.append(conditions);
+};
 
-  const updateState = function () {
-    samePassword();
-    containsCapitals();
-    aboveMinimalLength();
-    render();
-  };
+const updateState = function () {
+  // update state
+  samePassword();
+  containsCapitals();
+  aboveMinimalLength();
 
-  paragraphPasswordInputs.addEventListener('change', (event) => {
-    if (event.target == password || event.target == confirmPassword) {
-      updateState();
-    }
-  });
-})();
+  // update state view
+  clean();
+  render();
+};
+
+paragraphPasswordInputs.addEventListener('change', (event) => {
+  if (event.target == password || event.target == confirmPassword) {
+    updateState();
+  }
+});
